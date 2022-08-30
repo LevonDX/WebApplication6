d8c7e1e39fe836ed75fecb721cc166209bd93fe7
Levon Hovhannisyan <lehovhan@microsoft.com>
2022-08-30 10:49:24 +0400

    Add project files.

---
+++ WebApplication6/FoodData.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace WebApplication6
+{
+    public static class FoodData
+    {
+        /* public static IEnumerable<Food> Foods = new List<Food>
+         {
+             new Food { Name = "Cake", Price = 2.00m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Cookie", Price = 1.00m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Candy", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Ice Cream", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Chocolate", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Chips", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Bread", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Cake", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 },
+             new Food { Name = "Cookie", Price = 0.50m, ExpiationDate = "12.12.12", Quantity = 1 }
+         };
+        */
+
+
+        public static IEnumerable<Food> GetFoods()
+        {
+            using (StreamReader sr = new StreamReader(@"C:\Users\lehovhan\source\repos\WebApplication6\WebApplication6\Repository\Food.txt"))
+            {
+                string? line = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] items = line.Split(',');
+
+                    Food food = new Food()
+                    {
+                        Name = items[0],
+                        Price = decimal.Parse(items[1]),
+                        ExpiationDate = items[2],
+                        Quantity = int.Parse(items[3])
+                    };
+
+                    yield return food;
+                }
+            }
+        }
+    }
+}
